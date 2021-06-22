14ab85bceae7a9c64e99fc1dd05060c4cbeec48f
TongDucChung <tongvannhu12@gmail.com>
2021-06-22 08:14:36 +0700

    a

---
+++ src/com/company/GenericStackClient.java
@@ -0,0 +1,13 @@
+package com.company;
+import com.company.MyGenericStack;
+
+import static com.company.MyGenericStack.stackOfIntegers;
+
+public class GenericStackClient {
+    public static void main(String[] args) {
+        System.out.println("1. Stack of integers");
+        stackOfIntegers();
+        System.out.println("\n2. Stack of Strings");
+//        stackOfIStrings();
+    }
+}
